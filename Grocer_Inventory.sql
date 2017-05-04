f0aecbeb0b174f31ade6c558829a9a72e85f10a6
ColinKenney <colin-kenney@uiowa.edu>
2017-05-04 10:47:10 -0500

    Add files via upload

--- Grocer_Inventory.sql
+++ Grocer_Inventory.sql
@@ -8,13 +8,12 @@ CREATE TABLE inventory(
 	unit INT unsigned NOT NULL,
 	price DECIMAL(6,2) NOT NULL,
 	stock INT unsigned NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES orders(id)
-) AUTO_INCREMENT=1 ;
+	PRIMARY KEY(id)
+    
+);
 
 
 --data for inventory
 INSERT INTO inventory(image, category, product, unit, price, stock) VALUES ('http://www.aedairy.com/images/products/Whole-Milk-AE-gallon.jpg', 'Dairy', 'Whole Milk', '1', 7.50, 100);
 INSERT INTO inventory(image, category, product, unit, price, stock) VALUES ('http://lancastria.net/blog/wp-content/uploads/2014/07/corn.jpg', 'Produce', 'Corn', '1', 0.25, 250);
 INSERT INTO inventory(image, category, product, unit, price, stock) VALUES ('https://saltmarshrunning.com/wp-content/uploads/2014/09/bananasf.jpg', 'Fruit', 'Banana', '5', 3.99, 150);
-INSERT INTO inventory(image, category, product, unit, price, stock) VALUES ('https://www.mainelobsternow.com/images/live_lobster.jpg', 'Seafood', 'Lobster', '1', 7.99, 10;
--- orderedProducts.sql
+++ orderedProducts.sql
@@ -7,8 +7,5 @@ CREATE TABLE orderedProducts(
 	id INT NOT NULL AUTO_INCREMENT,
 	quantity INT unsigned NOT NULL,
 	PRIMARY KEY(id)
-    FOREIGN KEY(id) REFERENCES inventory(id)
-    FOREIGN KEY(price) REFERENCES inventory(price)
-    FOREIGN KEY(id) REFERENCES orders(id)
-    UNIQUE KEY(quantity)
+  
 );
