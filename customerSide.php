b797fb76b46a43430f3584889e5ceb537a0309d3
GefeiLiu <gefeiliu95@gmail.com>
2017-05-04 05:20:05 -0500

    based on previous code
    so it might not work with shopping cart

--- customerSide.php
+++ customerSide.php
@@ -1,130 +1,55 @@
-<html>
-	<head>
-<!-- Bootstrap link -->		
-		
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-		
-		<title>Grocery Hawk</title>
-	</head>
-	<body>
-		
-		
 <?php
-// check if the form data needs to be  processed
-// include config and utils files
-include_once('config.php');
-include_once('dbutils.php');
+include_once('header.php');
 ?>
 
-<!-- Menu bar -->
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav navbar-left">
-    	<li><a class="navbar-brand" href="#">Grocery Hawk</a></li>
-        <li class="active"><a href="customerSide.php">Inventory</a></li>
-        <li><a href="customerOrders.php">Orders</a></li>
-     </ul>
-     <ul class="nav navbar-nav navbar-right">
-<!-- Search bar -->
-		<li>
-		<!--form class="navbar-form" role="search">
-		<form action="" method="post">  
-		<input type="text" name="term" id=“term” /><br />  
-		<input type="submit" value="Submit" />  
-		</form-->
-		<form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search" name="term" id=“term”>
-        </div>
-        <button type="submit" class="btn btn-default">Go!</button>
-        </form>
-        <!--div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        </div-->
-        </form>
-		</li>
-		<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php 
-			session_start();
-			if(isset($_SESSION['email'])){
-				echo $_SESSION['email'];
-			}else{
-				if (!isset($_SESSION['email'])) {
-				session_destroy();
-			} 
-				echo "Guest ";
-			}
-		?><span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          <?php if(isset($_SESSION['email'])){ echo"
-            <li><a href='#'>Account Information</a></li>
-            <li><a href='#'>Order History</a></li>
-            <li><a href='#'>Order Status</a></li>
-            <li><a href='#'>Help</a></li>
-            <!--li role='separator' class='divider'></li-->";}
-            else{
-            	echo "<li><a href='customerLogin.php'>Please Login</a></li>";
-            }?>
-          </ul>
-        </li>
-        <li><a href="shopCart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
-		<li><?php 
-			session_start();
-		if(isset($_SESSION['email'])){
-			echo "<br/><li><a href='customerLogout.php'>Logout</a></li>";
-		}else{
-			if (!isset($_SESSION['email'])) {
-			session_destroy();
-		} 
-			echo "<br/><li><a href='customerLogin.php'>Login/Create Account</a></li>";
-		}
-		?></li>
-        <!--<li><a href="customerLogin.php">Login/Create Account</a></li>-->
-     </ul>
-  </div>
-</nav>
+<!-- View Cart Box Start -->
+<?php
+if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
+{
+	echo '<div class="cart-view-table-front" id="view-cart">';
+	echo '<h3>Your Shopping Cart</h3>';
+	echo '<form method="post" action="updateCart.php">';
+	echo '<table width="100%"  cellpadding="6" cellspacing="0">';
+	echo '<tbody>';
 
-<!-- Title -->	
-
-
-<!-- welcome user -->
-<!--div class="row">
-    <div class="col-xs-10">
-        <p><?php if(isset($_SESSION['email'])) echo 'Welcome:' . '&nbsp' . $_SESSION['email']; ?></p>
-    </div>
-</div-->
-
-
-<!-- show contents of items table -->
-<div class="row">
-	<div class="col-xs-12">
-	<div class="col-xs-1"></div>
-		
-<!-- Set up html table to show contents -->
-<div class="col-xs-10">
-<table class="table table-hover">
-	<!-- headers for table -->
-	<thead>
-		<th>Category</th>
-		<th>Product</th>
-		<th>Unit</th>
-		<th>Price</th>
-		<th>Stock</th>
-		<th></th>
-	</thead>
+	$total =0;
+	$b = 0;
+	foreach ($_SESSION["cart_products"] as $cart_itm)
+	{
+		$product = $cart_itm["product"];
+		$unit = $cart_itm["unit"];
+		$price = $cart_itm["price"];
+		$code = $cart_itm["code"];
+		$bg_color = ($b++%2==1) ? 'odd' : 'even'; //zebra stripe
+		echo '<tr class="'.$bg_color.'">';
+		echo '<td>Qty <input type="text" size="2" maxlength="2" name="unit['.$code.']" value="'.$unit.'" /></td>';
+		echo '<td>'.$product.'</td>';
+		echo '<td><input type="checkbox" name="remove_code[]" value="'.$code.'" /> Remove</td>';
+		echo '</tr>';
+		$subtotal = ($price * $unit);
+		$total = ($total + $subtotal);
+	}
+	echo '<td colspan="4">';
+	echo '<button type="submit">Update</button><a href="viewCart.php" class="button">Checkout</a>';
+	echo '</td>';
+	echo '</tbody>';
+	echo '</table>';
 	
+	$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+	echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
+	echo '</form>';
+	echo '</div>';
+
+}
+?>
+<!-- View Cart Box End -->
+
+
+			<div class="row" id="grid"></div>
+			<table class="table table-hover">
+				<!-- headers for table -->
+
+<!-- Products List Start -->
 <?php
 	/*
 	 *List all products in the database
@@ -133,61 +58,91 @@ include_once('dbutils.php');
 	
 	
 	//conect to the database
-	$db = connectDB($DBHost,$DBUser, $DBPasswd, $DBName);
-	$term = $_GET['term']; 
+	$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+	$term = $_GET['term'];
 	
 	if (!empty($term)) {
-
-	//$term = mysql_real_escape_string($_REQUEST['term']);
-
-
-	$query = "SELECT inventory.category, inventory.product, inventory.unit, inventory.price, inventory.stock, inventory.id FROM inventory WHERE inventory.product LIKE '%".$term."%'"; 
-
-	$result = queryDB($query, $db);
-
-
-	while($row = nextTuple($result)) {
 		
-		echo "\n <tr>";
-		echo "<td>" . $row['category'] . "</td>";
-		echo "<td>" . $row['product'] . "</td>";
-		echo "<td>" . $row['unit'] . "</td>";
-		echo "<td>" . $row['price'] . "</td>";
-		echo "<td>" . $row['stock'] . "</td>";
-		echo "<td><a href='shopCart.php.php?id=" . $row['id']  .  "'>Add to Cart</a></td>";
-		echo "</tr> \n";
-	}
-
-	}
-	
 	// set up a query to get info on the cars form the db
-	else{
-	$query = "SELECT inventory.category, inventory.product, inventory.unit, inventory.price, inventory.stock, inventory.id FROM inventory;";
-
+	$query = "SELECT inventory.id, inventory.category, inventory.product, inventory.unit, inventory.price, inventory.stock FROM inventory WHERE inventory.product LIKE '%".$term."%' or inventory.category LIKE '%".$term."%'";
+	
 	//run the query
 	$result = queryDB($query, $db);
 	
-	while($row = nextTuple($result)) {
-		
-		echo "\n <tr>";
-		echo "<td>" . $row['category'] . "</td>";
-		echo "<td>" . $row['product'] . "</td>";
-		echo "<td>" . $row['unit'] . "</td>";
-		echo "<td>" . $row['price'] . "</td>";
-		echo "<td>" . $row['stock'] . "</td>";
-		echo "<td><a href='shopCart.php.php?id=" . $row['id']  .  "'>Add to Cart</a></td>";
-		echo "</tr> \n";
-	}
-}
 	
+while($row = nextTuple($result)) {
+					
+					echo "\n <tr><td><div class='col-sm-12'>";
+					//echo "<td>" . $row['category'] . "</td>";
+					echo "<div class='col-sm-1'></div><div class='col-sm-2'><img src=". $row['image'] ."alt='' border='2' height='180' width='180' /></div>";
+					echo "<div class='col-sm-2'><div>" . $row['product'] . "</div>";
+					echo "<div> $" . $row['price'] . "</div><div>" . $row['image'] . "</div></div>";
+					echo "<div class='form-group row'>
+								<div class='col-sm-2'>
+								<label for='sel1' class='control-label'>Quantity:</label>
+								</div>
+								<div class='col-sm-2'>
+								<select class='form-control id='sel1'>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+								</select>
+								<a style='margin-top:2em' href='updateInventory.php?id=" . $row['id']  .  "'>Add to cart</a>
+								</div>
+								</div>";
+					// link to update record (item)
+			        //echo "<div class='col-sm-2'><a href='updateInventory.php?id=" . $row['id']  .  "'>Add to cart</a></div>";
+					// link to delete record (item)
+					echo "</div></td></tr> \n";
+					
+				}
+
+	}
+	
+	else{
+// set up a query to get info on the cars form the db
+	$query = "SELECT inventory.id, inventory.category, inventory.product, inventory.unit, inventory.price, inventory.stock FROM inventory;";
+	
+	//run the query
+	$result = queryDB($query, $db);
+	
+	
+while($row = nextTuple($result)) {
+					
+					echo "\n <tr><td><div class='col-sm-12'>";
+					//echo "<td>" . $row['category'] . "</td>";
+					echo "<div class='col-sm-1'></div><div class='col-sm-2'><img src=". $row['image'] ."alt='' border='2' height='180' width='180' /></div>";
+					echo "<div class='col-sm-2'><div>" . $row['product'] . "</div>";
+					echo "<div> $" . $row['price'] . "</div><div>" . $row['image'] . "</div></div>";
+					echo "<div class='form-group row'>
+								<div class='col-sm-2'>
+								<label for='sel1' class='control-label'>Quantity:</label>
+								</div>
+								<div class='col-sm-2'>
+								<select class='form-control id='sel1'>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+								</select>
+								<a style='margin-top:2em' href='updateInventory.php?id=" . $row['id']  .  "'>Add to cart</a>
+								</div>
+								</div>";
+					// link to update record (item)
+			        //echo "<div class='col-sm-2'><a href='updateInventory.php?id=" . $row['id']  .  "'>Add to cart</a></div>";
+					// link to delete record (item)
+					echo "</div></td></tr> \n";
+					
+				}
+
+	}
 	
 ?>
-	</table>
-	<?php if (!empty($term)) echo "<a href='customerSide.php' class='btn btn-default glyphicon glyphicon-menu-left'>Back</a>";?>
-	</div>
-	<div class="col-xs-1"></div>
-	</div>
-</div>
-
-	</body>
+</table>
+<?php if (!empty($term)) echo "<a href='customerSide.php' class='btn btn-default glyphicon glyphicon-menu-left'>Back</a>";?>
+<!-- Products List End -->
+</body>
 </html>
---
+++ header.php
@@ -0,0 +1,121 @@
+<html>
+	<head>
+<!-- Bootstrap link -->		
+		
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<title>Grocery Hawk</title>
+	</head>
+	<body>
+		
+		
+<?php
+// check if the form data needs to be  processed
+// include config and utils files
+include_once('config.php');
+include_once('dbutils.php');
+?>
+<img src="gh.jpg" height=auto width="1240">
+<!-- Menu bar -->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav navbar-left">
+        <li><a href="customerSide.php"><span class="glyphicon glyphicon-grain"></span> Inventory</a></li>
+        <li><a href="viewCart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
+     </ul>
+     <ul class="nav navbar-nav navbar-right">
+     	<!--li>
+     		<div style="display:table;" class="input-group">
+            <span style="width: 1%;" class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
+            <input type="text" autofocus="autofocus" autocomplete="off" placeholder="Search Here" name="search" style="" class="form-control">
+          </div-->
+     	</li>
+		<li>
+		<form class="navbar-form navbar-right" role="search">
+			<div class="form-group">
+				<input style="width:680px;" type="text" class="form-control" placeholder="Search by product or category" name="term" id=“term”>
+				</div>
+			<button type="submit" class="btn btn-default">Go!</button>
+			</form>
+		</li>
+		<li style="margin-left: 0.4em;" class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php
+			session_start();
+			if(isset($_SESSION['email'])){
+				echo $_SESSION['email'];
+			}else{
+				if (!isset($_SESSION['email'])) {
+					session_destroy();
+				}
+				echo "Guest ";
+			}
+			?><span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			<?php if(isset($_SESSION['email'])){ echo"
+			<li><a href='Account.php'>Account Information</a></li>
+            <li><a href='OrderC.php'>Order History</a></li>
+            <li><a href='#'>Help</a></li>
+            <!--li role='separator' class='divider'></li-->";}
+             else{
+				echo "<li><a href='customerLogin.php'>Please Login</a></li>";
+			}?>
+			</ul>
+			</li>		
+		<li><?php 
+		session_start();
+		if(isset($_SESSION['email'])){
+
+			echo "<a href='customerLogout.php'><span class='glyphicon glyphicon-log-out'></span> Log Out</a>";
+		}else{
+			if (!isset($_SESSION['email'])) {
+			session_destroy();
+		} 
+			echo "<br/><li><a href='customerLogin.php'>Login/Create Account</a></li>";
+		}
+		?></li>
+        <!--<li><a href="customerLogin.php">Login/Create Account</a></li>-->
+     </ul>
+  </div>
+</nav>
+
+<style type="text/css">
+        body {
+			background: rgb(255,255,255); /* Old browsers */
+background: -moz-linear-gradient(top, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 48%, rgba(186,186,186,1) 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(top, rgba(255,255,255,1) 0%,rgba(255,255,255,1) 48%,rgba(186,186,186,1) 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(255,255,255,1) 48%,rgba(186,186,186,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#bababa',GradientType=0 ); /* IE6-9 */
+            text-align: center;
+        }
+        .table {
+    margin: 0px auto;
+}
+
+#grid td {
+    text-align: center;
+    font-size: 0.8em;
+    padding-top: -5em;
+}
+
+div {
+	margin-bottom: 0.5em;
+}
+
+#quantity {
+	margin-top: 0.5em;
+	margin-right: -2em;
+	padding-left: 8em;
+}
+
+}
+
+</style>
+
+
